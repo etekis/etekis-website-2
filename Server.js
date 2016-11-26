79c7cf72ba6e789fd080a778f21a21de149ac1a4
Emre Tekisalp <emre.tekisalp@admins-MacBook-Air.local>
2016-11-26 14:48:46 -0500

    initial commit

---
+++ Server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var app = express()
+var router = express.Router();
+var path = __dirname+'/views/'
+
+app.set('port',(process.env.PORT || 5000))
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+app.use(express.static('assets'))
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(app.get('port'),function(){
+  console.log("App is running on", app.get('port'));
+});
